667bc6092a4a0a2740b75f809554dee791049070
aravv17 <avyawahare@ucsd.edu>
2024-10-12 00:18:16 +0000

    1st

--- simple.c
+++ simple.c
@@ -2,16 +2,23 @@
 
 int my_strlen(char str[]) {
     // Write this together
+    int length = 0;
+
+    for (int i=0; str[i] != '\0'; i++) {
+        length++;
+    }
+    return length;
 }
 
 int main() {
     char input[256];
-
+    char emoji[5] = "🐐";
     // take user input
-    printf("Enter a string: ");
+    // printf("Enter a string: ");
     // WHAT GOES HERE?
 
     // call my_strlen and print length
     // WHAT IS MY FORMAT SPECIFIER?
-    printf("", my_strlen(input));
+    // printf("", my_strlen(input));
+    printf("🐐");
 }
